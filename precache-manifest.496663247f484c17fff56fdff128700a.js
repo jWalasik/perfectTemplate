5bbdd75258cbf26ffa5577a2ac011195517c0a68
Jacek Walasik <jacekwalasik89@gmail.com>
2019-05-27 23:03:17 +0200

    Updates

---
+++ precache-manifest.496663247f484c17fff56fdff128700a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1d5d0f06b5b275a7df235765ac36197",
+    "url": "/perfectTemplate/static/media/shield.d1d5d0f0.png"
+  },
+  {
+    "revision": "be6da2a5eae78906ebeed8a43922a538",
+    "url": "/perfectTemplate/static/media/placeholder2.be6da2a5.jpg"
+  },
+  {
+    "revision": "31a97680fef228fbf558b32add13f1ca",
+    "url": "/perfectTemplate/static/media/cogs.31a97680.png"
+  },
+  {
+    "revision": "e67a8ae33d3272f767f11a26961a7fe8",
+    "url": "/perfectTemplate/static/media/book.e67a8ae3.png"
+  },
+  {
+    "revision": "7f327d61a1c0c9af7394",
+    "url": "/perfectTemplate/static/js/runtime~main.e88b8b49.js"
+  },
+  {
+    "revision": "ce58cb8e5670d3ae2245",
+    "url": "/perfectTemplate/static/js/main.0184672b.chunk.js"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/perfectTemplate/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "ce58cb8e5670d3ae2245",
+    "url": "/perfectTemplate/static/css/main.29eac0b8.chunk.css"
+  },
+  {
+    "revision": "3d4e7340f286a5874cecfb3bf307104a",
+    "url": "/perfectTemplate/index.html"
+  }
+];
